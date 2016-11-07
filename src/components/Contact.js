123a41994c0aed2542f4d3b8db32a87f112df0ec
gywl81 <gywl81@spTek.co.kr>
2016-11-07 16:32:25 +0900

    검색/ 클릭 기능 추가

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
 
-export default class ContactInfo extends React.Component {
+export default class Contact extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+        	selectedKey: -1,
+        	keyword: '',
+            contactData: [{
+                name: 'Abet',
+                phone: '010-0000-0001'
+            }, {
+                name: 'Betty',
+                phone: '010-0000-0002'
+            }, {
+                name: 'Charlie',
+                phone: '010-0000-0003'
+            }, {
+                name: 'David',
+                phone: '010-0000-0004'
+            }]
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleChange(e) {
+    	this.setState({
+    		keyword: e.target.value
+    	});
+    }
+    
+    handleClick(key) {
+    	this.setState({
+    		selectedKey: key
+    	});
+    	console.log (key);
+    }
+    
     render() {
+        const mapToComponents = (data) => {
+        	data.sort();
+        	data= data.filter(
+        			(contact) => {
+        				return contact.name.indexOf(this.state.keyword) > -1;
+        				/*return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;*/
+        			}
+        	)
+            return data.map((contact, i) => {
+                return (<ContactInfo
+                		contact={contact}
+                		key={i}
+                		onClick={() => this.handleClick(i)}/>);
+            });
+        };
+        
         return (
-            <div>{this.props.contact.name} {this.props.contact.phone}</div>
+            <div>
+                <h1>Contacts</h1>
+                <input
+                	name="keyword"
+                	placeholder="Search"
+                	value={this.state.keyword}
+                	onChange={this.handleChange}
+                />
+                <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails 
+                	isSelected={this.state.selectedKey != -1}
+                	contact={this.state.contactData[this.state.selectedKey]}/>
+            </div>
         );
     }
 }
