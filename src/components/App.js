0e1b35e3dce33cceb51dda4c0fa48edad37d9c06
gywl81 <gywl81@spTek.co.kr>
2016-11-07 15:37:39 +0900

    수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-				name: '' 
-		};
-	}
-	render() {
-		return (
-			<div>
-				<button onClick={()=> {this.setState({name: 'gywl81'});}}>Click me</button>
-				<h1>hello!!! {this.state.name}</h1>
-			</div>
-		);
-	}
+    render(){
+        return (
+                <Contact/>
+        );
+    }
 }
 
 export default App;
