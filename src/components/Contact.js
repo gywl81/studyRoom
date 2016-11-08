f20bd4dfd538bca7d77c1792117053e69b5a3dc5
gywl81 <gywl81@spTek.co.kr>
2016-11-08 18:12:45 +0900

    insert

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import ContactCreate from './ContactCreate';
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
     
@@ -26,6 +28,10 @@ export default class Contact extends React.Component {
         
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
     
     handleChange(e) {
@@ -38,9 +44,32 @@ export default class Contact extends React.Component {
     	this.setState({
     		selectedKey: key
     	});
-    	console.log (key);
+    	console.log (key + 'is selected');
     }
     
+    handleCreate(contact) {
+    	this.setState({
+    		contactData: update(this.state.contactData, {$push: [contact]})
+    	});
+    }
+    handleRemove() {
+    	this.setState({
+    		contactData: update(this.state.contactData, {$splice: [[this.state.selectedKey, 1]]}),
+    		selectedKey: -1
+    	});
+    }
+    handleEdit(name, phone) {
+    	this.setState({
+    		contactData: update(this.state.contactData,
+    			{
+	    			[this.state.selectedKey]: {	    			
+	    				name: {$set: name},
+	    				phone: {$set: phone}
+	    			}
+    			}
+    		)
+    	});
+    }
     render() {
         const mapToComponents = (data) => {
         	data.sort();
@@ -71,7 +100,10 @@ export default class Contact extends React.Component {
                 <ContactDetails 
                 	isSelected={this.state.selectedKey != -1}
                 	contact={this.state.contactData[this.state.selectedKey]}/>
-            </div>
+                <ContactCreate
+                	onCreate={this.handleCreate}
+                />
+           </div>
         );
     }
 }
